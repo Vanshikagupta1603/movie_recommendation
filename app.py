2e795c831d92c59efacca82e4df1b0c8e3c0d5d3
Vanshika Gupta <vanshikaguptaalw@gmail.com>
2026-06-08 18:32:10 +0530

    Update Streamlit app and ignore large similarity.pkl

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-# %pip install streamlit
 import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+@st.cache_data
+def load_movies():
+    movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+    return pd.DataFrame(movies_dict)
 
-    recommended_movies = []
-    for i in movies_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+@st.cache_data
+def build_similarity(df):
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(df['tags'].astype('str'))
+    return cosine_similarity(tfidf_matrix)
 
-
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity.pkl','rb'))
+movies = load_movies()
+similarity = build_similarity(movies)
 
 st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
+    'Choose a movie',
     movies['title'].values)
+
 if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+    movie_index = movies[movies['title'] == selected_movie_name].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = [movies.iloc[i[0]].title for i in movies_list]
+    for recommended_movie in recommended_movies:
+        st.write(recommended_movie)
 
